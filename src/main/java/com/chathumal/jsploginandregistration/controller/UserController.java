48f6bf5edacf0808ed6e9ee8605c59062734b296
chathumal <chathumaljayasingha@hotmail.coim>
2022-03-25 05:06:13 +0530

    Initial commit

---
+++ src/main/java/com/chathumal/jsploginandregistration/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.chathumal.jsploginandregistration.controller;
+
+import com.chathumal.jsploginandregistration.entity.User;
+import com.chathumal.jsploginandregistration.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author Chathumal Jayasingha | [chathumaljayasingha@hotmail.com]
+ * @since - 3/25/22 | 2022-March-25[Friday]
+ */
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
+        userValidator.validate(userForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+
+        userService.save(userForm);
+
+        return "redirect:/welcome";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
+    }
+
+    @GetMapping({"/", "/welcome"})
+    public String welcome(Model model) {
+        return "welcome";
+    }
+
+
+}
